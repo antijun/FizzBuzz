10e50b20db911725f47cc2b415ee8ec544f8bd23
antijun <davidwu361@gmail.com>
2023-09-13 01:29:52 -0400

    fixed multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,7 +2,7 @@ public class Multiples {
     public static void main(String[] args) {
         int numMultiples = 0;
         for (int i = 1; i < 1000; i++){
-            if ((i % 3 == 0) & (i % 5 == 0)){
+            if (i % 3 == 0 || i % 5 == 0){
                 numMultiples++;
             }
         }
